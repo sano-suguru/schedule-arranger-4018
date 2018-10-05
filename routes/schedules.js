116c416e54e5c3a83ff52ca1b7753278e22dded5
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-05 04:53:59 +0000

    スケジュールの保存処理を作成

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,14 +2,36 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnusurer = require('./authentication-ensurer');
+const uuid = require('node-uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
 
 router.get('/new', authenticationEnusurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
 router.post('/', authenticationEnusurer, (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
+  Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255),
+    memo: req.body.memo,
+    createdBy: req.user.id,
+    updatedAt: updatedAt
+  }).then((schedule) => {
+    const candidateNames = req.body.candidates.trim()
+      .split('\n')
+      .map(s => s.trim())
+      .filter(s => s !== '');
+    const candidates = candidateNames.map(c => ({
+      candidateName: c,
+      scheduleId: schedule.scheduleId
+    }));
+    Candidate.bulkCreate(candidates).then(() => {
+      res.redirect('/schedules/' + schedule.scheduleId);
+    });
+  });
 });
 
 module.exports = router;
